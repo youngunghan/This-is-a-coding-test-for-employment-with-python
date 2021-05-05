0bc6dc083b1a1da59fcceea6d3c355ca11bef29b
Youngung Han <hyulives@gmail.com>
2021-05-06 03:29:32 +0900

    Update number card game.py

--- 숫자 카드 게임_98p/number card game.py
+++ 숫자 카드 게임_98p/number card game.py
@@ -1,1 +1,18 @@
-print("hi")
+def read_data():
+    with open('숫자 카드 게임_98p\input.txt', 'r') as f:
+        datas = []
+        while True:
+            line = f.readline()
+            if not line:
+                break            
+            datas.append( list(map(int, line.split())) )
+        return datas
+
+def main():
+    datas = read_data()
+    max_rows = []
+    for row_data in datas[1:]:
+        max_rows.append(min(row_data))
+    print(max(max_rows))
+
+main()
